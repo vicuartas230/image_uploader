23f2455278e7a899afde3e385b414ee4c5de40e4
vicuartas230 <vicuartas230@gmail.com>
2023-07-13 06:01:52 +0930

    Update database and create new app

--- image_uploader_back/image_uploader_back/settings.py
+++ image_uploader_back/image_uploader_back/settings.py
@@ -37,11 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
+    'images'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'image_uploader_back.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "image_uploader_database",
+        "USER": "image_uploader_database_user",
+        "PASSWORD": "tNU0I8JThUf4NsWq8Mn02Dbh3zEA4W0D",
+        "HOST": "dpg-cinfladph6ei90e96cv0-a.singapore-postgres.render.com",
+        "PORT": "5432",
     }
 }
 
@@ -121,3 +129,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS authorization
+
+CORS_ALLOWED_ORIGINS = []
